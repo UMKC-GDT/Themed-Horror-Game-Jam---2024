a4a745596861b7634dd74fdf820c8fe6ba689e70
umkcVR <cgch6w@umsystem.edu>
2024-10-28 19:04:32 -0500

    Ethan Fire Place Changes

---
+++ Themed Horror Game Jam 2024/Assets/BurnTheItems.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnTheItems : MonoBehaviour
+{
+    public string[] objectsToBurn; //write in the specific name of the object
+    private ParticleSystem fire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fire = GameObject.Find("FireParticles").GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        for(int i = 0; i < objectsToBurn.Length; i++)
+        {
+            if(other.gameObject.name == objectsToBurn[i] && !other.gameObject.GetComponent<PickUpObject>().hasItem)
+            {
+                //Set your event or whatever you want here
+                Destroy(other.gameObject);
+                var main = fire.main;
+                main.startSpeed = 3f;
+                StartCoroutine(normalFire());
+                break;
+            }
+        }
+
+    }
+
+    IEnumerator normalFire()
+    {
+        Debug.Log("Fire Check");
+        // suspend execution for 1 seconds
+        yield return new WaitForSeconds(2f);
+        var main = fire.main;
+        main.startSpeed = 1f;
+        Debug.Log("Fire Check Complete");
+
+    }
+}
