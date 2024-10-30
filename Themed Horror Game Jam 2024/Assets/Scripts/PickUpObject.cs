688b2f7ca796073b6f8ec2822fbec5f4f8f5dd62
umkcVR <cgch6w@umsystem.edu>
2024-10-29 19:09:04 -0500

    Fixed pickup code

--- Themed Horror Game Jam 2024/Assets/Scripts/PickUpObject.cs
+++ Themed Horror Game Jam 2024/Assets/Scripts/PickUpObject.cs
@@ -30,7 +30,7 @@ public class PickUpObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canpickup == true) // if you enter thecollider of the objecct
+        if (canpickup == true && myHands.transform.childCount == 0) // if you enter thecollider of the objecct
         {
             msg = getGuiMsg(hasItem);
             if (Input.GetKeyDown(KeyCode.Q))  // can be e or any key
@@ -41,6 +41,7 @@ public class PickUpObject : MonoBehaviour
                 }
 
                 ObjectIwantToPickUp.GetComponent<Rigidbody>().isKinematic = true;   //makes the rigidbody not be acted upon by forces
+                ObjectIwantToPickUp.GetComponent<Collider>().enabled = false;
                 ObjectIwantToPickUp.transform.position = myHands.transform.position; // sets the position of the object to your hand position
                 ObjectIwantToPickUp.transform.parent = myHands.transform; //makes the object become a child of the parent so that it moves with the hands
                 hasItem = true;
@@ -51,7 +52,7 @@ public class PickUpObject : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R) && hasItem == true) // if you have an item and get the key to remove the object, again can be any key
         {
             ObjectIwantToPickUp.GetComponent<Rigidbody>().isKinematic = false; // make the rigidbody work again
-
+            ObjectIwantToPickUp.GetComponent<Collider>().enabled = false;
             ObjectIwantToPickUp.transform.parent = null; // make the object no be a child of the hands
             hasItem = false;
             msg = getGuiMsg(hasItem);
@@ -59,7 +60,7 @@ public class PickUpObject : MonoBehaviour
     }
      void OnTriggerEnter(Collider other) // to see when the player enters the collider
     {
-        if (other.CompareTag("Player")) //on the object you want to pick up set the tag to be anything, in this case "object"
+        if (other.CompareTag("Player") ) //on the object you want to pick up set the tag to be anything, in this case "object"
         {
             Debug.Log("tag");
             canpickup = true;  //set the pick up bool to true
