c642c5bdb7accb523fa83deffc127d4bb1f46a2b
umkcVR <100800088+Joseph-Sailor-Usher@users.noreply.github.com>
2024-10-29 20:16:59 -0500

    Update burn the item

--- Themed Horror Game Jam 2024/Assets/Scripts/BurnTheItems.cs
+++ Themed Horror Game Jam 2024/Assets/Scripts/BurnTheItems.cs
@@ -6,8 +6,7 @@ using System.Collections.Generic;
 public class BurnTheItems : MonoBehaviour
 {
     public List<GameObject> objectsToBurn, wrongItemsToBurn;
-    private ParticleSystem fire;
-    public Color badFire, goodFire;
+    public ParticleSystem fire, goodFire;
     public UnityEvent onItemBurned, onWrongItemBurned;
 
     void Start()
@@ -23,9 +22,10 @@ public class BurnTheItems : MonoBehaviour
             onItemBurned.Invoke();
             Debug.Log("Correct");
             Destroy(other.gameObject);
+            goodFire.Play();
+            fire.Stop();
             var main = fire.main;
             main.startSpeed = 3f;
-            main.startColor = goodFire;
             StartCoroutine(normalFire());
         }
         if(other.gameObject.CompareTag("NoBurn") && !other.gameObject.GetComponent<PickUpObject>().hasItem)
@@ -34,9 +34,9 @@ public class BurnTheItems : MonoBehaviour
             onWrongItemBurned.Invoke();
             Debug.Log("Incorrect");
             Destroy(other.gameObject);
+            fire.Play();
             var main = fire.main;
             main.startSpeed = 3f;
-            main.startColor = badFire;
             StartCoroutine(normalFire());
         }
     }
@@ -46,9 +46,10 @@ public class BurnTheItems : MonoBehaviour
         Debug.Log("Fire Check");
         // suspend execution for 1 seconds
         yield return new WaitForSeconds(2f);
+        goodFire.Stop();
+        fire.Play();
         var main = fire.main;
         main.startSpeed = 1f;
-        main.startColor = badFire;
         Debug.Log("Fire Check Complete");
     }
 }
