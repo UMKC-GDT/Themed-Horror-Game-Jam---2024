b0eb4aa65c5ebe2e6b9846589882d3e6cbe540fc
umkcVR <100800088+Joseph-Sailor-Usher@users.noreply.github.com>
2024-11-08 17:22:00 -0600

    Updated lighting and copied the main scene as a reference

--- Themed Horror Game Jam 2024/Assets/Scripts/SettingsManager.cs
+++ Themed Horror Game Jam 2024/Assets/Scripts/SettingsManager.cs
@@ -1,5 +1,6 @@
-using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine;
+using System;
 
 public class SettingsManager : MonoBehaviour
 {
@@ -9,7 +10,6 @@ public class SettingsManager : MonoBehaviour
     private const string BrightnessPrefKey = "Brightness";
     private const string VolumePrefKey = "Volume";
 
-    // Static properties to access brightness and volume
     public static float Brightness
     {
         get => PlayerPrefs.GetFloat(BrightnessPrefKey, 1.0f);
@@ -43,17 +43,17 @@ public class SettingsManager : MonoBehaviour
 
     private void OnBrightnessChanged(float value)
     {
-        Brightness = value;  // Update static property, which saves to PlayerPrefs
+        Brightness = value;
+        RenderSettings.ambientIntensity = value;
     }
 
     private void OnVolumeChanged(float value)
     {
-        Volume = value;  // Update static property, which saves to PlayerPrefs
+        Volume = value;
     }
 
     private void OnDisable()
     {
-        // Ensure values are saved when the object is disabled
         PlayerPrefs.Save();
     }
 }
